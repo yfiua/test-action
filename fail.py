603b3cea6c8d0889d1e7784e4ecf8437ec057100
Jun S <yfiua6@gmail.com>
2025-08-30 20:38:58 +0200

    add scripts

---
+++ fail.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# This script exits with a non-zero status code to indicate failure.
+
+import sys
+
+sys.exit(1)
---
+++ success.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# This script exits with a success status code.
+
+import sys
+
+sys.exit(0)
